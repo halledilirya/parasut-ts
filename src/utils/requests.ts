5068797d12b00e63c60a2f9858a61acf54d88502
halledilirya <yusnujs@gmail.com>
2022-11-25 19:59:42 +0300

    max depth for requests

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -5,11 +5,14 @@ export const send = async ({
   path,
   method,
   body,
+  depth = 0,
 }: {
   path: string;
   method: string;
   body?: any;
+  depth?: number;
 }) => {
+  if (depth > 3) throw new Error("Maximum depth reached");
   const { accessToken } = await validateAccessToken();
   const { base_url, firm_id } = global.user;
   const response = await new Promise((resolve, reject) => {
@@ -23,7 +26,7 @@ export const send = async ({
     })
       .then(async (res) => {
         if (res.status === 401) {
-          return await send({ path, method, body });
+          return await send({ path, method, body, depth: depth + 1 });
         }
         if (method === "DELETE") {
           res.status === 204 ? resolve({}) : resolve(await res.json());
@@ -38,20 +41,31 @@ export const send = async ({
   return response;
 };
 
-// export class Requests {
-//   constructor() {}
+// export class Request {
+//   private _tokenConfig: AccessTokenSuccessResponse | undefined = undefined;
 
-//   async get({ url }: { url: string }) {
-//     const tokenRes = token.findOne({ name: "token_info" });
-//     if (!tokenRes) {
-//       throw new Error("Token not found");
-//     }
-//     const response = await new Promise((resolve, reject) => {
-//       fetch(tokenRes.base_url + url, {
-//         method: "GET",
+//   constructor(private _userConfig: ParasutConfig) {}
+
+//   async getAccessToken(): Promise<AccessTokenSuccessResponse> {
+//     console.log("GET ACCESS TOKEN");
+//     const { email, password, client_id, client_secret, redirect_uri } =
+//       this._userConfig;
+//     const config = {
+//       username: email,
+//       password,
+//       client_id,
+//       client_secret,
+//       redirect_uri,
+//     };
+//     const response = (await new Promise((resolve, reject) => {
+//       fetch(this._userConfig.base_url + "/oauth/token", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           ...config,
+//           grant_type: "password",
+//         }),
 //         headers: {
 //           "Content-Type": "application/json",
-//           ...(token && { Authorization: `Bearer ${token}` }),
 //         },
 //       })
 //         .then(async (res) => {
@@ -60,75 +74,96 @@ export const send = async ({
 //         .catch((err) => {
 //           resolve(err);
 //         });
-//     });
+//     })) as AccessTokenResponse;
+
+//     if ("error" in response) {
+//       throw new Error(response.error_description);
+//     }
+
+//     this._tokenConfig = {
+//       ...response,
+//     };
+
 //     return response;
 //   }
 
-//   async post({ url, data }: { url: string; data: any }) {
-//     const tokenRes = token.findOne({ name: "token_info" });
-//     if (!tokenRes) {
-//       throw new Error("Token not found");
+//   async validateAccessToken(): Promise<{
+//     accessToken: string;
+//   }> {
+//     if (!this._tokenConfig) {
+//       const res = await this.getAccessToken();
+//       return { accessToken: res.access_token };
 //     }
-//     const accessToken = await validateAccessToken();
+//     const { access_token, refresh_token, expires_in, created_at } =
+//       this._tokenConfig;
+//     this._tokenConfig;
+//     const now = Math.floor(Date.now() / 1000);
+
+//     if (
+//       created_at &&
+//       expires_in &&
+//       access_token &&
+//       now - created_at < expires_in - 7150
+//     ) {
+//       return { accessToken: access_token };
+//     }
+
+//     const { client_id, client_secret } = this._userConfig;
+//     const response = await fetch(this._userConfig.base_url + "/oauth/token", {
+//       method: "POST",
+//       body: JSON.stringify({
+//         grant_type: "refresh_token",
+//         refresh_token,
+//         client_id,
+//         client_secret,
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+
+//     if (!response.ok) {
+//       const response = await this.getAccessToken();
+//       return {
+//         accessToken: response.access_token,
+//       };
+//     }
+
+//     const data = (await response.json()) as AccessTokenSuccessResponse;
+//     this._tokenConfig = {
+//       ...data,
+//     };
+//     return {
+//       accessToken: data.access_token,
+//     };
+//   }
+
+//   async send({
+//     path,
+//     method,
+//     body,
+//   }: {
+//     path: string;
+//     method: string;
+//     body?: any;
+//   }) {
+//     const { accessToken } = await this.validateAccessToken();
+//     const { base_url, firm_id } = this._userConfig;
 //     const response = await new Promise((resolve, reject) => {
-//       fetch(tokenRes.base_url + url, {
-//         method: "POST",
-//         body: JSON.stringify(data),
+//       fetch(`${base_url}/v4/${firm_id}` + path, {
+//         method,
 //         headers: {
 //           "Content-Type": "application/json",
 //           Authorization: `Bearer ${accessToken}`,
 //         },
+//         ...(body && { body: JSON.stringify(body) }),
 //       })
 //         .then(async (res) => {
-//           resolve(await res.json());
-//         })
-//         .catch((err) => {
-//           resolve(err);
-//         });
-//     });
-//     return response;
-//   }
-
-//   async put({ url, data }: { url: string; data: any }) {
-//     const tokenRes = token.findOne({ name: "token_info" });
-//     if (!tokenRes) {
-//       throw new Error("Token not found");
-//     }
-//     const response = await new Promise((resolve, reject) => {
-//       fetch(tokenRes.base_url + url, {
-//         method: "PUT",
-//         body: JSON.stringify(data),
-//         headers: {
-//           "Content-Type": "application/json",
-//           ...(token && { Authorization: `Bearer ${token}` }),
-//         },
-//       })
-//         .then(async (res) => {
-//           resolve(await res.json());
-//         })
-//         .catch((err) => {
-//           resolve(err);
-//         });
-//     });
-//     return response;
-//   }
-
-//   async delete({ url }: { url: string }) {
-//     const tokenRes = token.findOne({ name: "token_info" });
-//     if (!tokenRes) {
-//       throw new Error("Token not found");
-//     }
-//     const response = await new Promise((resolve, reject) => {
-//       fetch(tokenRes.base_url + url, {
-//         method: "DELETE",
-//         headers: {
-//           "Content-Type": "application/json",
-//           ...(token && { Authorization: `Bearer ${token}` }),
-//         },
-//       })
-//         .then(async (res) => {
-//           if (res.status === 204) {
-//             resolve({ status: "success" });
+//           if (res.status === 401) {
+//             return await this.send({ path, method, body });
+//           }
+//           if (method === "DELETE") {
+//             res.status === 204 ? resolve({}) : resolve(await res.json());
 //           } else {
 //             resolve(await res.json());
 //           }
